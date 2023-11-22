8da4b97e2f4ffbaf74f580cee0dbf988600a7525
denizgursoy <deniz.gursoy@ingenico.com>
2023-11-22 22:40:21 +0100

    create mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/denizgursoy/genum
+
+go 1.21.1
+
+require github.com/dave/jennifer v1.7.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	. "github.com/dave/jennifer/jen"
+)
+
+func main() {
+	f := NewFile("main")
+	f.Func().Id("main").Params().Block(
+		Qual("fmt", "Println").Call(Lit("Hello, world")),
+	)
+}
